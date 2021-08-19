0226fdbc2a401086390ce04e089a701189ba3f36
abe <abe@project-studio.cc>
2021-08-19 06:20:18 +0000

    [update] 將影城資訊改為爬蟲抓取

--- GetAmbassadorTimeTable.py
+++ GetAmbassadorTimeTable.py
@@ -3,81 +3,60 @@ import datetime
 import re
 import json
 from bs4 import BeautifulSoup
+from urllib.parse import parse_qs
+import urllib.parse as urlparse
 import time 
 import os
 
 # https://www.ambassador.com.tw/home/TheaterList      #影城頁面
-theaterInfos = [ #國賓影城 (全台)
-    {
-        'name': '國賓大戲院',
-        'id': '84b87b82-b936-4a39-b91f-e88328d33b4e',
-        'datesTable': []
-    },
-    {
-        'name': '台北微風國賓影城',
-        'id': '5c2d4697-7f54-4955-800c-7b3ad782582c',
-        'datesTable': []
-    },
-    {
-        'name': '台北長春國賓影城',
-        'id': '453b2966-f7c2-44a9-b2eb-687493855d0e',
-        'datesTable': []
-    },
-    {
-        'name': '新莊晶冠國賓影城',
-        'id': '3301d822-b385-4aa8-a9eb-aa59d58e95c9',
-        'datesTable': []
-    },
-    {
-        'name': '林口昕境國賓影城',
-        'id': '9383c5fa-b4f3-4ba8-ba7a-c25c7df95fd0',
-        'datesTable': []
-    },
-    {
-        'name': '淡水禮萊國賓影城',
-        'id': '1e42d235-c3cf-4f75-a382-af60f67a4aad',
-        'datesTable': []
-    },
-    {
-        'name': '八德廣豐國賓影城',
-        'id': '8fda9934-73d4-4c14-b1c4-386c2b81045c',
-        'datesTable': []
-    },
-    {
-        'name': '台中忠孝國賓影城',
-        'id': '7008eb95-aa95-4cb1-9530-2551382cf26f',
-        'datesTable': []
-    },
-    {
-        'name': '台南國賓影城',
-        'id': 'ace1fe19-3d7d-4b7c-8fbe-04897cbed08c',
-        'datesTable': []
-    },
-    {
-        'name': '高雄義大國賓影城',
-        'id': 'ec07626b-b382-474e-be39-ad45eac5cd1c',
-        'datesTable': []
-    },
-    {
-        'name': '高雄草衙道國賓影城',
-        'id': 'f760950a-94b6-4e04-9573-831ed7283c5c',
-        'datesTable': []
-    },
-    {
-        'name': '屏東環球國賓影城',
-        'id': '41aae717-4464-49f4-ac26-fec2d16acbd6',
-        'datesTable': []
-    },
-    {
-        'name': '金門昇恆昌國賓影城',
-        'id': '65db51ce-3ad5-48d8-8e32-7e872e56aa4a',
-        'datesTable': []
-    }
-]
+# theaterInfos = [ # init data structure
+#     {
+#         'crawler_theater_name': '國賓大戲院', 
+#         'crawler_theater_id': '84b87b82-b936-4a39-b91f-e88328d33b4e', 
+#         'address': '台北市成都路88號', 
+#         'tel': '02-2361-1223', 
+#         'datesTable': []
+#     }
+# ]
 
-theater_length = len(theaterInfos)
-# print(theater_length)
-# today_date = time.strftime("%Y/%m/%d", time.localtime())
+# 匯出到指定資料夾的 function, 還需測試 
+# def write_json(target_path, target_file, data): 
+#     if not os.path.exists(target_path):
+#         try:
+#             os.makedirs(target_path)
+#         except Exception as e:
+#             print(e)
+#             raise
+#     with open(os.path.join(target_path, target_file), 'w', encoding='utf-8') as f:
+#         json.dump(data, f, ensure_ascii=False,  indent = 2)
+
+
+"""
+取得影城資訊及 ID
+Return:
+    array: array of theater infos
+"""
+def getTheaterInfos():
+    theaterInfos = []
+    r_theater = requests.get('https://www.ambassador.com.tw/home/TheaterList')
+    soup_theater = BeautifulSoup(r_theater.text, "html.parser")
+    theater_cells = soup_theater.select('div.theater > div.cell')
+    for cell in theater_cells:
+        theater_info_block = cell.find('div', class_="theater-info")
+        a_tag_href = cell.find("a").get('href')
+        parsed_url = urlparse.urlparse(a_tag_href)
+        theater_ID = parse_qs(parsed_url.query)['ID'][0]
+        theater_name = theater_info_block.find('h6').text
+        theater_address = theater_info_block.find('p').text
+        theater_phone = theater_info_block.find('p').find_next_sibling('p').text
+        theaterInfos.append({
+            'crawler_theater_name': theater_name,
+            'crawler_theater_id': theater_ID,
+            'address': theater_address,
+            'tel': theater_phone,
+            'datesTable': []
+        })
+    return theaterInfos
 
 """
 This is get next few date function
@@ -94,9 +73,8 @@ def getDates(n):
     t_year = x.year
     t_month = x.strftime("%m")
     t_day = x.strftime("%d")
-    today_date = f"{str(t_year)}/{str(t_month)}/{str(t_day)}"
+    today_date = time.strftime("%Y/%m/%d", time.localtime())
     today_after_6_days.append(today_date)
-    # print(today_after_6_days)
     # n = 5  #選擇後 5 天, 將日期存成 ary
     for x in range(n):
         d = x + 1
@@ -105,36 +83,25 @@ def getDates(n):
         today_after_6_days.append(time_format)
     return today_after_6_days
 
-# def write_json(target_path, target_file, data):
-#     if not os.path.exists(target_path):
-#         try:
-#             os.makedirs(target_path)
-#         except Exception as e:
-#             print(e)
-#             raise
-#     with open(os.path.join(target_path, target_file), 'w', encoding='utf-8') as f:
-#         json.dump(data, f, ensure_ascii=False,  indent = 2)
-
-# with open('Ambassador.json', 'w', encoding='utf-8') as f:
-#     json.dump(theaterInfos, f, ensure_ascii=False,  indent = 2)
 
 #https://www.ambassador.com.tw/home/Showtime?ID=84b87b82-b936-4a39-b91f-e88328d33b4e&DT=2021/08/13
 #取得包含今日之後的 6 天日期 ex: 2021/08/13
 # for loop theaters
     #for loop dates
-
-dates = getDates(5) 
+theaterInfos = getTheaterInfos()
+dates = getDates(5) # 取得包含今日的六天日期
 today = dates[0]
 print(dates)
+print(theaterInfos)
 for theater in theaterInfos:
-    print(f"======================================= {theater['name']} ==============================================")
+    print(f"======================================= {theater['crawler_theater_name']} ==============================================")
     for d in dates:
-        r = requests.get(f"https://www.ambassador.com.tw/home/Showtime?ID={theater['id']}&DT={d}")
+        r = requests.get(f"https://www.ambassador.com.tw/home/Showtime?ID={theater['crawler_theater_id']}&DT={d}")
         soup = BeautifulSoup(r.text, "html.parser")
         movies = soup.find_all('div', class_='showtime-item')
         # print(movies)
         if(len(movies) == 0): # 若當日無電影則繼續下一天
-            print(theater['name'] + ', ' + str(d) + '無電影' )
+            print(theater['crawler_theater_name'] + ', ' + str(d) + '無電影' )
             continue
 
         moviesInfo = {
@@ -190,9 +157,9 @@ for theater in theaterInfos:
         theater['datesTable'].append(moviesInfo) 
         # break
 
-print('OVER! Well Done')
-# write_json('/json', 'Ambassador.json', theaterInfos)
+print('Crawler finished! Well Done')
+# write_json('/json', 'Ambassador.json', theaterInfos) #未測試
 with open('Ambassador.json', 'w', encoding='utf-8') as f:
     json.dump(theaterInfos, f, ensure_ascii=False,  indent = 2)
-print('Write over')
+print('JSON dump finished')
 ################################################################
